8d05c04cae25ab88d6ebeb60481b70956f0e19d1
vobozin <vobozin@spbtv.com>
2022-02-05 18:19:35 +0300

    [no-jira] add first filter in api

--- api/app/models/card.rb
+++ api/app/models/card.rb
@@ -3,5 +3,11 @@
 class Card < ApplicationRecord
   # Represents banking card
   #
+  scope :filter_by_interest_rate, ->(interest_rate) { where interest_rate: interest_rate }
+  # scope :filter_by_credit_limit, ->(location_id) { where location_id: location_id }
+  # scope :filter_by_free_period, ->(name) { where("name like ?", "#{name}%") }
+  # scope :filter_by_maintenance_price_year, ->(name) {}
+  # scope :filter_by_maintenance_price_month, ->(name) {}
+  # scope :filter_by_maintenance_cash, ->(name) {}
 end
 
